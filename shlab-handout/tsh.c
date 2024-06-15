6779e0df535020417c060853ae5756b1f63ffa0f
Lianyi <1403805367@qq.com>
2024-06-15 23:30:45 +0800

    final

--- shlab-handout/tsh.c
+++ shlab-handout/tsh.c
@@ -399,15 +399,30 @@ void do_bgfg(char **argv)
  */
 void waitfg(pid_t pid)
 {
-
-    sigset_t mask;
+    /*
+    sigset_t mask, prev_mask;
     sigemptyset(&mask);
-    int ret = 0;
-    while(!ret)
-        sigsuspend(&mask);
-    
+    sigaddset(&mask, SIGCHLD);
+    while(1){
+        sigprocmask(SIG_BLOCK, &mask, &prev_mask);
+        int ret = fgpid(jobs);
+        while(ret==pid)
+            sigsuspend(&prev_mask);
+        sigprocmask(SIG_SETMASK, &prev_mask, NULL);
+    }
     return;
-
+    */
+    sigset_t mask_none,prev_all;
+    sigemptyset(&mask_none);
+    while(1)
+    {
+        sigprocmask(SIG_SETMASK,&mask_none,&prev_all);
+        int ret = fgpid(jobs);
+        if(!ret) break;
+        sleep(1);
+        sigprocmask(SIG_SETMASK,&prev_all,NULL);
+    }
+    return;
 }
 
 /*****************
@@ -715,5 +730,3 @@ void sigquit_handler(int sig)
     printf("Terminating after receipt of SIGQUIT signal\n");
     exit(1);
 }
-
-
